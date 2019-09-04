48084de407ba48cb5b120b7a2cf3edce993505dc
Saajan <saajan.sn@gmail.com>
2019-09-05 02:06:07 +0530

    api endpoints update for register and login user

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,54 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express api' });
 });
 
 router.post('/auth/loginuser', function (req, res, next) {
-    res.send({
+    console.log(req.body);
+    if (req.body.userName === 'vendorsin@gmail.com') {
+        res.json({
+            "responseStatus": true,
+            "responseMessage": "Success",
+            "loginToken": "MzAwMzkkVmVuZG9yVVNFUiBBVVRIIFRPS0VO",
+            "userName": "vendorsin",
+            "userId": 30039,
+            "mobileNumber": "9999999999",
+            "emailId": "vendorsin@gmail.com",
+            "userRole": "Vendor",
+            "roleId": 1
+        });
+    } else {
+        res.json({
+            "responseStatus": false,
+            "responseMessage": "User does not exist"
+        });
+    }
+});
+
+router.post('/auth/registeruser', function (req, res, next) {
+    if (req.body.emailId === 'vendorsin@gmail.com') {
+        res.json({
+            "responseStatus": true,
+            "responseMessage": "OTP has generated"
+        });
+    } else {
+        res.json({
+            "responseStatus": false,
+            "responseMessage": "User already Registered"
+        });
+    }
+});
+
+router.post('/otp/requestotp', function (req, res, next) {
+    res.json({
+        "responseStatus": false,
+        "responseMessage": "User already Registered"
+    });
+});
+
+router.post('/otp/validateotp', function (req, res, next) {
+    res.json({
         "responseStatus": false,
         "responseMessage": "User already Registered"
     });
