b8d31ae6270a75fa128ccd8ac09c50fc3a0d1783
Saajan <saajan.sn@gmail.com>
2019-09-24 02:16:21 +0530

    new upload fix

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 var proxy = require('express-http-proxy');
+var bodyParser = require('body-parser')
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
 
@@ -14,12 +15,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(cors()) 
+app.use(cors())
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: 2000000}));
+app.use(express.urlencoded({limit: 2000000, extended: false}));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -27,12 +29,12 @@ app.use('/api', apiRouter);
 app.use('/proxy', proxy('http://vendorsinapi.7iron.in'));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
